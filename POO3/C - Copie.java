8daaff074ba18336329903248768d9ea67989918
URIEL237477 <UrielEtoundi10@outlook.COM>
2025-05-10 14:15:35 +0100

    classe de lexercice 13

---
+++ POO3/C - Copie.java
@@ -0,0 +1,4 @@
+public class C {
+    public char ch = 'C';
+    public char getCh() { return ch; }
+   }
---
+++ POO3/D.java
@@ -0,0 +1,5 @@
+public class D extends C {
+    public char ch = 'D';
+    public char getCh() { return ch; }
+   }
+   
